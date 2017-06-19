ed15693c998341067e502c2abe3a8033fa97ea16
CarloLucibello <carlo.lucibello@gmail.com>
2017-06-19 16:16:38 +0200

    remove some imports

--- src/ErdosExtras.jl
+++ src/ErdosExtras.jl
@@ -3,16 +3,21 @@ module ErdosExtras
 using Erdos
 using JuMP # Matching
 using GLPKMathProgInterface
-using Cbc
-using Clp
-using Gurobi
 
-export minimum_weight_perfect_bmatching,
-       solve_tsp
+export set_lp_solver, set_mip_solver, get_lp_solver, get_mip_solver
+
+export minimum_weight_perfect_bmatching
+
+export solve_tsp
 
 LP_SOLVER = GLPKSolverLP()   #GurobiSolver(OutputFlag=0)
 MIP_SOLVER = GLPKSolverMIP()
 
+set_lp_solver(solv) = (LP_SOLVER=solv; solv)
+set_mip_solver(solv) = (MIP_SOLVER=solv; solv)
+
+get_lp_solver() = LP_SOLVER
+get_mip_solver() = MIP_SOLVER
 
 include("matching/Matching.jl")
 include("tsp/TSP.jl")
