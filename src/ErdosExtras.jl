4140c71f14332b701a6c6efae4a912d1554891a8
CarloLucibello <carlo.lucibello@gmail.com>
2017-06-19 16:17:32 +0200

    comment non working code

--- src/ErdosExtras.jl
+++ src/ErdosExtras.jl
@@ -1,10 +1,10 @@
-# __precompile__()
+__precompile__()
 module ErdosExtras
 using Erdos
-using JuMP # Matching
+using JuMP
 using GLPKMathProgInterface
 
-export set_lp_solver, set_mip_solver, get_lp_solver, get_mip_solver
+# export set_lp_solver, set_mip_solver, get_lp_solver, get_mip_solver
 
 export minimum_weight_perfect_bmatching
 
@@ -13,11 +13,11 @@ export solve_tsp
 LP_SOLVER = GLPKSolverLP()   #GurobiSolver(OutputFlag=0)
 MIP_SOLVER = GLPKSolverMIP()
 
-set_lp_solver(solv) = (LP_SOLVER=solv; solv)
-set_mip_solver(solv) = (MIP_SOLVER=solv; solv)
-
-get_lp_solver() = LP_SOLVER
-get_mip_solver() = MIP_SOLVER
+# set_lp_solver(solv) = (LP_SOLVER=solv; solv)
+# set_mip_solver(solv) = (MIP_SOLVER=solv; solv)
+#
+# get_lp_solver() = LP_SOLVER
+# get_mip_solver() = MIP_SOLVER
 
 include("matching/Matching.jl")
 include("tsp/TSP.jl")
